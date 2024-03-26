42db48a9efda7231d10997f57ce7aeb7ec90cc1d
Anurag Gaiwal <anuraggaiwal0@gmail.com>
2024-03-26 10:04:35 +0530

    manifest.json add

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
     
       <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="../public/manifest.json" />
         <meta name="theme-color" content="#000000" />
       </head>
